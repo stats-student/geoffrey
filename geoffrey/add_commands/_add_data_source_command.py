e987fdc57e8df4057cfc950113563bd5b41c93ff
stats-student <112397903+stats-student@users.noreply.github.com>
2022-09-01 19:23:07 +0100

    adding a seperate class for the add command

---
+++ geoffrey/add_commands/_add_data_source_command.py
@@ -0,0 +1,13 @@
+"""
+This script contains all the functions required for the add data source command
+
+Author: Daniel Yates
+"""
+import logging
+
+logger = logging.getLogger(__name__)
+
+class AddDataSourceCommand:
+
+    def __init__(self) -> None:
+        pass
